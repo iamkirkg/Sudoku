4408b2ca9d45cecf3540b9301f1e822cd788e17d
Kirk Glerum <kirk@glerum.com>
2020-03-24 16:00:35 -0700

    My own Sudoku tic-tac-toe form

---
+++ SudokuForms/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SudokuForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.q03 = new System.Windows.Forms.TableLayoutPanel();
+            this.q06 = new System.Windows.Forms.TableLayoutPanel();
+            this.q30 = new System.Windows.Forms.TableLayoutPanel();
+            this.q33 = new System.Windows.Forms.TableLayoutPanel();
+            this.q36 = new System.Windows.Forms.TableLayoutPanel();
+            this.q60 = new System.Windows.Forms.TableLayoutPanel();
+            this.q63 = new System.Windows.Forms.TableLayoutPanel();
+            this.q66 = new System.Windows.Forms.TableLayoutPanel();
+            this.q00 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Controls.Add(this.q03, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.q06, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.q30, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.q33, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.q36, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.q60, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.q63, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.q66, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.q00, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1569, 1141);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // q00
+            // 
+            this.q00.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q00.ColumnCount = 3;
+            this.q00.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q00.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q00.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q00.Location = new System.Drawing.Point(3, 3);
+            this.q00.Name = "q00";
+            this.q00.RowCount = 3;
+            this.q00.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q00.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q00.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q00.Size = new System.Drawing.Size(516, 372);
+            this.q00.TabIndex = 0;
+            this.q00.Paint += new System.Windows.Forms.PaintEventHandler(this.q00_Paint);
+            // 
+            // q03
+            // 
+            this.q03.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q03.ColumnCount = 3;
+            this.q03.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q03.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q03.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q03.Location = new System.Drawing.Point(525, 3);
+            this.q03.Name = "q03";
+            this.q03.RowCount = 3;
+            this.q03.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q03.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q03.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q03.Size = new System.Drawing.Size(486, 372);
+            this.q03.TabIndex = 1;
+            this.q03.Paint += new System.Windows.Forms.PaintEventHandler(this.q03_Paint);
+            // 
+            // q06
+            // 
+            this.q06.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q06.ColumnCount = 3;
+            this.q06.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q06.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q06.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q06.Location = new System.Drawing.Point(1047, 3);
+            this.q06.Name = "q06";
+            this.q06.RowCount = 3;
+            this.q06.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q06.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q06.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q06.Size = new System.Drawing.Size(496, 372);
+            this.q06.TabIndex = 2;
+            this.q06.Paint += new System.Windows.Forms.PaintEventHandler(this.q06_Paint);
+            // 
+            // q30
+            // 
+            this.q30.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q30.ColumnCount = 3;
+            this.q30.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q30.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q30.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q30.Location = new System.Drawing.Point(3, 383);
+            this.q30.Name = "q30";
+            this.q30.RowCount = 3;
+            this.q30.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q30.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q30.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q30.Size = new System.Drawing.Size(516, 372);
+            this.q30.TabIndex = 3;
+            this.q30.Paint += new System.Windows.Forms.PaintEventHandler(this.q30_Paint);
+            // 
+            // q33
+            // 
+            this.q33.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q33.ColumnCount = 3;
+            this.q33.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q33.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q33.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q33.Location = new System.Drawing.Point(525, 383);
+            this.q33.Name = "q33";
+            this.q33.RowCount = 3;
+            this.q33.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q33.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q33.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q33.Size = new System.Drawing.Size(486, 372);
+            this.q33.TabIndex = 4;
+            this.q33.Paint += new System.Windows.Forms.PaintEventHandler(this.q33_Paint);
+            // 
+            // q36
+            // 
+            this.q36.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q36.ColumnCount = 3;
+            this.q36.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q36.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q36.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q36.Location = new System.Drawing.Point(1047, 383);
+            this.q36.Name = "q36";
+            this.q36.RowCount = 3;
+            this.q36.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q36.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q36.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q36.Size = new System.Drawing.Size(496, 372);
+            this.q36.TabIndex = 5;
+            this.q36.Paint += new System.Windows.Forms.PaintEventHandler(this.q36_Paint);
+            // 
+            // q60
+            // 
+            this.q60.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q60.ColumnCount = 3;
+            this.q60.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q60.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q60.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q60.Location = new System.Drawing.Point(3, 763);
+            this.q60.Name = "q60";
+            this.q60.RowCount = 3;
+            this.q60.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q60.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q60.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q60.Size = new System.Drawing.Size(516, 361);
+            this.q60.TabIndex = 6;
+            this.q60.Paint += new System.Windows.Forms.PaintEventHandler(this.q60_Paint);
+            // 
+            // q63
+            // 
+            this.q63.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q63.ColumnCount = 3;
+            this.q63.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q63.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q63.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q63.Location = new System.Drawing.Point(525, 763);
+            this.q63.Name = "q63";
+            this.q63.RowCount = 3;
+            this.q63.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q63.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q63.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q63.Size = new System.Drawing.Size(486, 361);
+            this.q63.TabIndex = 7;
+            this.q63.Paint += new System.Windows.Forms.PaintEventHandler(this.q63_Paint);
+            // 
+            // q66
+            // 
+            this.q66.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.q66.ColumnCount = 3;
+            this.q66.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q66.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q66.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q66.Location = new System.Drawing.Point(1047, 763);
+            this.q66.Name = "q66";
+            this.q66.RowCount = 3;
+            this.q66.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q66.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.q66.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.q66.Size = new System.Drawing.Size(496, 361);
+            this.q66.TabIndex = 8;
+            this.q66.Paint += new System.Windows.Forms.PaintEventHandler(this.q66_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1592, 1167);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel q00;
+        private System.Windows.Forms.TableLayoutPanel q03;
+        private System.Windows.Forms.TableLayoutPanel q06;
+        private System.Windows.Forms.TableLayoutPanel q30;
+        private System.Windows.Forms.TableLayoutPanel q33;
+        private System.Windows.Forms.TableLayoutPanel q36;
+        private System.Windows.Forms.TableLayoutPanel q60;
+        private System.Windows.Forms.TableLayoutPanel q63;
+        private System.Windows.Forms.TableLayoutPanel q66;
+    }
+}
+
---
+++ SudokuForms/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void q36_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q00_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q30_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q63_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q60_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q03_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q06_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q33_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void q66_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
