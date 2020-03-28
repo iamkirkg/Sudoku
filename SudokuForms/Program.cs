c4e00bbe4b34c17c3f3d30951bbf5ee06a16ead4
Kirk Glerum <kirk@glerum.com>
2020-03-27 19:36:36 -0700

    Form3 is with buttons

--- SudokuForms/Program.cs
+++ SudokuForms/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using GameEngine;
+
 namespace SudokuForms
 {
     static class Program
@@ -17,7 +19,31 @@ namespace SudokuForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new Form2());
+
+            //BoardLayout.Square foo;
+
+            BoardLayout.Square[,] GameBoard = new BoardLayout.Square[9,9]
+            {
+                { new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(2), new BoardLayout.Square(2), new BoardLayout.Square(2) },
+                { new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(2), new BoardLayout.Square(2), new BoardLayout.Square(2) },
+                { new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(0), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(1), new BoardLayout.Square(2), new BoardLayout.Square(2), new BoardLayout.Square(2) },
+                { new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(5), new BoardLayout.Square(5), new BoardLayout.Square(5) },
+                { new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(5), new BoardLayout.Square(5), new BoardLayout.Square(5) },
+                { new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(3), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(4), new BoardLayout.Square(5), new BoardLayout.Square(5), new BoardLayout.Square(5) },
+                { new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(8), new BoardLayout.Square(8), new BoardLayout.Square(8) },
+                { new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(8), new BoardLayout.Square(8), new BoardLayout.Square(8) },
+                { new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(6), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(7), new BoardLayout.Square(8), new BoardLayout.Square(8), new BoardLayout.Square(8) }
+            };
+
+            for (int i = 0; i <= 8; i++)
+                {
+                int j = GameBoard[0, i].q;
+                Console.WriteLine(j);
+                };
+
+            //Application.Run(new Form2());
+
+            Application.Run(new Form3());
         }
     }
 }
