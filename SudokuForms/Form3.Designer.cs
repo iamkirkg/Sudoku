530a6b32419a45cb358e809e0bdcf126c35efa5b
Kirk Glerum <kirk@glerum.com>
2020-03-28 16:28:12 -0700

    Click dem buttons

--- SudokuForms/Form3.Designer.cs
+++ SudokuForms/Form3.Designer.cs
@@ -37,6 +37,8 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,6 +61,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "1 2 3 4 5 6 7 8 9";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -69,6 +72,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "1 2 3 4 5 6 7 8 9";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -79,6 +83,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "1 2 3 4 5 6 7 8 9";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -89,6 +94,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "1 2 3 4 5 6 7 8 9";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -99,6 +105,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "1 2 3 4 5 6 7 8 9";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -109,6 +116,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "1 2 3 4 5 6 7 8 9";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -119,6 +127,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "1 2 3 4 5 6 7 8 9";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -129,12 +138,36 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "1 2 3 4 5 6 7 8 9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(349, 32);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(112, 72);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Step";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(349, 148);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(112, 72);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Go";
+            this.button11.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 516);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -161,5 +194,7 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
